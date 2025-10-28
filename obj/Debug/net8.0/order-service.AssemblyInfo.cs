bba8c5536e3a4129de407b74f3f2858caa6947cf
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2025-10-28 12:17:06 -0300

    feat: added order proto

--- obj/Debug/net8.0/order-service.AssemblyInfo.cs
+++ obj/Debug/net8.0/order-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("order-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d4d0762beb30cc208c6e2db22eb48f7ed4a4a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8c8cd3536b31f23820f56513ae5747cef73765")]
 [assembly: System.Reflection.AssemblyProductAttribute("order-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("order-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
