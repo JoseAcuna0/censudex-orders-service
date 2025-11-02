75a61812edbeeb60625c4fc0c514184022b7189b
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2025-11-01 22:20:30 -0300

    feat: added database conection in program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Register gRPC
+
 builder.Services.AddGrpc();
 
-// Register your domain service (currently in-memory)
 builder.Services.AddScoped<IOrderService, OrderService>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -20,7 +19,6 @@ builder.Services.AddDbContext<OrderDbContext>(options =>
 
 var app = builder.Build();
 
-// Map the gRPC service
 app.MapGrpcService<OrderGrpcServiceImpl>();
 
 app.Run();
