97d88e6aff1dd25d3bdf6eef812898e326f69312
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2025-11-13 23:48:20 -0300

    refactor: added customerEmail to order model

--- src/Models/Order.cs
+++ src/Models/Order.cs
@@ -11,6 +11,8 @@ namespace order_service.src.Models
         public Guid CustomerId { get; set; }
         public string CustomerName { get; set; } = string.Empty;
 
+        public string CustomerEmail { get; set; } = string.Empty;
+
         public DateTime OrderDate { get; set; }
 
         public decimal TotalAmount { get; set; }
